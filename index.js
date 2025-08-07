cb679fe760c0f890e542d644c662060807a3d6a9
lancelot7608 <prom366@gmail.com>
2025-08-07 08:20:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Reader from './Reader';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Reader />
+  </React.StrictMode>
+);
