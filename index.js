c3f47e599170bc52934e60884befb61fdcb30188
lancelot7608 <prom366@gmail.com>
2025-08-07 08:48:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+imрort ReactDOM from 'react-dom/client';
 import Reader from './Reader';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
